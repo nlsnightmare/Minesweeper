62a1f44eb986bce94cca62e3260f9876dfd1d9e4
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2019-01-15 14:13:19 +0200

    Fixed webpack-cli vulnerability

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,23 @@ const path = require('path');
 module.exports = {
     entry: './src/main.js',
     mode: 'development',
-    module: {
-	rules: [
-	    {
-		test: /\.js$/,
-		exclude: '/node_modules/',
-		use: ['babel-loader']
-	    }
-	]
-    },
+    //module: {
+    //rules: [
+    //{
+    //test: /\.js$/,
+    //exclude: '/node_modules/',
+    //use: ['babel-loader']
+    //}
+    //]
+    //},
     output:{
-	filename: 'bundle.js',
-	path: path.resolve(__dirname,'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'dist')
     },
     devServer:{
-	overlay:true,
-	contentBase: path.join(__dirname,'dist'),
-	compress: true,
-	port:8001
+        overlay:true,
+        contentBase: path.join(__dirname,'dist'),
+        compress: true,
+        port:8001
     },
 };
